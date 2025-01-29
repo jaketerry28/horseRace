932735a4412f1bb3f5f425310999fe2ef51e2ddb
Jake Terry <47571311+jaketerry28@users.noreply.github.com>
2025-01-29 01:34:57 +0000

    got isWinner and advance working

--- horseRace.cpp
+++ horseRace.cpp
@@ -7,35 +7,52 @@
 const int TRACKLENGTH = 15;
 const int RACERS = 5;
 
-void advance(int* horses);
+void advance(int horseNum, int* horses);
 //void printLane(int horseNum, int* horses);
-//bool isWinner(int horseNum, int* horses);
-
+bool isWinner(int horseNum, int* horses);
+void horsePos(int* horses);
 int main(){
-	
-	int horses[] = {0, 0, 0, 0, 0};
-	std::cout << "Before: " <<  &horses << std::endl;
-	for (int i = 0; i < RACERS; i++){
-		std::cout << i << std::endl;
-	} // end for
 
-	advance(horses);
-	
-	std::cout << "After: " << horses << std::endl;
-	for (int i = 0; i < RACERS; i++){
-		std::cout << i << std::endl;
-	} // end for
+	int horses[] = {0, 0, 0, 0, 0};
+
+	bool winner = false;
+
+	int counter = 0;
+
+	while (!winner){
+		counter++;
+		std::cout << "LOOP: " << counter << std::endl;
+		for (int i = 0; i < RACERS; i++){
+			//std::cout << horses[i] << std::endl;
+			advance(i, horses);
+			if (winner = isWinner(i, horses)){
+				std::cout << "winner is: " << i << std::endl;
+				winner = true;
+			return winner;
+			} // end if
+		} // end for
+		horsePos(horses);
+		std::cout << std::endl;
+	} // end while
 	return(0);
 } // end main
 
-void advance(int* horses){
+void advance(int horseNum, int* horses){
 	int coin = -1;
 	// random integer generator between 0 and 1
 	std::random_device rd;
 	std::uniform_int_distribution<int> dist(0, 1);
+	coin = dist(rd);
 
-	for (int i = 0; i < RACERS; i++){
-		coin = dist(rd);
-		&horses[i] += coin;
-	} // end for
+	horses[horseNum] += coin;
 } // end advance
+
+bool isWinner(int horseNum, int* horses){
+	return horses[horseNum] >= TRACKLENGTH + 1;
+} // end isWinner
+
+void horsePos(int* horses){
+	for (int i = 0; i < RACERS; i++){
+		std::cout << horses[i] << " ";
+	} // end for
+} // end horsePos
